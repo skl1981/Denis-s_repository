18d3e0ea24f173ac478a77b95a0c5228f341e240
Skl1981 <skl81@tut.by>
2020-03-24 16:54:56 +0300

    config

--- PS_Remoting_Config.ps1
+++ PS_Remoting_Config.ps1
@@ -1,1 +1,5 @@
-Enable-PSRemoting
+Enable-PSRemoting -SkipNetworkProfileCheck
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value *
+$Password = ConvertTo-SecureString '12#QWEasd' -AsPlainText -Force;
+$Credential = New-Object -TypeName pscredential -ArgumentList "Administrator", $Password;
+Enter-PSSession -ComputerName 192.168.137.5 -Credential $Credential 
