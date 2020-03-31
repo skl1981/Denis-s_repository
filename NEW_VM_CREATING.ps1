b7c4c873eb1ed50b5ee069c1a4dcdfdc122d1c6b
Skl1981 <skl81@tut.by>
2020-03-31 20:02:29 +0300

    config

--- NEW_VM_CREATING.ps1
+++ NEW_VM_CREATING.ps1
@@ -2,7 +2,10 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]
-    $VM_Name
+    $VM_Name,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $VM_Switch
       )
 Get-VMSwitch * | Format-Table Name
 New-VM -Name $VM_Name -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath C:\Virtual\VMs\$VM_Name.vhdx -Path C:\Virtual\VMData -NewVHDSizeBytes 30GB -Generation 1 -SwitchName Skakalski_Virtual_Switch_internal
